490bfef300b5ce1eca088afc08190660d2b0c609
unknown <90sidort@gmail.com>
2020-07-04 14:05:32 +0200

    added navbar, plugins, reactstrapp, bootstrap

---
+++ src/pages/author.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const AuthorPage = () => (
+  <Layout>
+    <SEO title="Autor" />
+    <h1>Autor</h1>
+  </Layout>
+)
+
+export default AuthorPage
